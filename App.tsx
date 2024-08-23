daa7579d1429eef9ef9c568ddb9b8340bf9aa19c
nikkk404 <nikhil20mokal@gmail.com>
2024-08-23 20:03:46 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image ,SafeAreaView} from 'react-native';
+//to avoid text on notch we use SafeAreaView
+//Image component is use to use image in application
+
 
 export default function App() {
+  //here we have created funtion instead on writing console.log in onPress event
+  const handlePress = () => (console.log("Text Clicked")); 
   return (
-    <View style={styles.container}>
+   /* <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */
+
+    //to avoid text on notch we use SafeAreaView
+    <SafeAreaView style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    </SafeAreaView>
+
+    //numberOfLines is use to wrap the text in one line
+    //onPress is use to run a funtion on press on something
+    /*<SafeAreaView style={styles.container}>
+    <Text numberOfLines={1} onPress={() => console.log("Text Clicked")}>Open up App.tsx to start working on your app!Abc Qwe fvf sf gbrs vtbr vdg dg wr nj occ e bbt  sgb </Text>
+    </SafeAreaView>*/
+
+    //Here we have replaced console.log in onPress by funtion
+    /*<SafeAreaView style={styles.container}>
+    <Text numberOfLines={1} onPress={handlePress}>Open up App.tsx to start working on your app!Abc Qwe fvf sf gbrs vtbr vdg dg wr nj occ e bbt  sgb </Text>
+    </SafeAreaView>*/
   );
 }
 
@@ -14,7 +35,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",//align to center
+    alignItems:"center",//align to center
   },
 });
